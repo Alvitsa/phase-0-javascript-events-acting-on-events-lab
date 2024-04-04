f9929550f29ffca22480b792304e2c2018d6fad8
+ <+>
2024-04-04 12:14:47 +0300

    creates lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+// Define the moveDodgerLeft function
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const currentPosition = parseInt(dodger.style.left);
+
+    if (currentPosition > 0) {
+        dodger.style.left = `${currentPosition - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  })
+
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const currentPosition = parseInt(dodger.style.left);
+
+    if (currentPosition > 0) {
+        dodger.style.left = `${currentPosition + 1}px`;
+    }
+}
